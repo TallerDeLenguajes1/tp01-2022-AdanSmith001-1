e5fcc5bb2ae60a9e0d39852710249f3bbcaf71c0
Adan Smith Paredes Solorzano <asps.23329@gmail.com>
2022-03-23 01:23:48 -0300

    trabajo practico 01

--- tp1_1.c
+++ tp1_1.c
@@ -2,6 +2,16 @@
 
 int main ()
 {
-    printf("Hola Mundo");
+    printf("Hola Mundo\n");
+
+    int *punt, num;
+    num = 1;
+    punt = &num;
+
+    printf("\nContenido de la variable puntero: %p\n", punt);
+    printf("Direcci%cn de memoria almacenada en el puntero: %p\n", 162, punt);
+    printf("Direcci%cn de memoria de la variable: %p\n",162, &num);
+    printf("Direcci%cn de memoria del puntero: %p\n",162, &punt);
+    printf("Tama%co de memoria utilizado por el puntero: %d",164, sizeof(punt));
     return 0;
 }
